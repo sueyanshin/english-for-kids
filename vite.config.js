511953b2e516a8cdf40ac12c4f9ba40c07d7c90e
sueyanshin <sueyanshin@gmail.com>
2024-03-05 08:29:42 +0630

    routes added

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,16 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
+
         level1: resolve(__dirname, "level1/lvl1.html"),
         topics: resolve(__dirname, "level1/topics.html"),
         videos: resolve(__dirname, "level1/videos.html"),
         audiobooks: resolve(__dirname, "level1/audiobooks.html"),
+        songs: resolve(__dirname, "level1/songs.html"),
         makeFlashcard: resolve(__dirname, "makeFlashcard/index.html"),
+
+        alphabets: resolve(__dirname, "level1/alphabets.html"),
+        animals: resolve(__dirname, "level1/animals.html"),
       },
     },
   },
